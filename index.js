c07069c5b4f20fc70d9bc135f3c25624100e591a
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2023-10-17 03:51:16 +0900

    feat: :sparkles: x.comにも対応
    混在にも対応した。はず。

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-require ('date-utils');
+// require ('date-utils');
 
 const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages ]})
-const date = new Date();
-const currentTime = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+// const date = new Date();
+// const currentTime = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+const currentTime = "temporally disabled";
 
 client.once(Events.ClientReady, c => {
   console.log("--------------------");
@@ -22,28 +23,38 @@ client.on(Events.MessageCreate, message => {
     message.channel.send("aaa");
   }
 
-  if (message.content.includes("https://twitter.com")) {
+  if (message.content.includes("https://twitter.com"|"https://x.com")) {
 
-    m = message.content;
+    m = message.content.replace(/https:\/\/x.com/g, "https://twitter.com");
+    console.log(`[${currentTime}] Twitter URL found: ${m}`)
     rm = m.replace(/twitter.com/g, "fxtwitter.com");
-    console.log(`[${currentTime}] Twitter URL found. Replaced to fxtwitter.com: ${rm}`);
-
-    https://fxtwitter.com/SzlyNe_/status/1712654233160573425
-
-    srm = rm.match(/https:\/\/fxtwitter.com\/[a-zA-Z_]*\/status\/[0-9]*/g);
+    console.log(`[${currentTime}] Replaced to fxtwitter.com: ${rm}`);
+    srm = rm.match(/https:\/\/fxtwitter.com\/[a-zA-Z0-9_]*\/status\/[0-9]*/g);
     console.log(`[${currentTime}] Replaced Message preview: ${srm}`);
 
     tssrm = srm.toString();
     frtssrm = tssrm.replace(/,/g, "\n");
 
-    // console.log(frtssrm);
-    message.reply({content: "found twitter links! replaced urls:\n" + frtssrm, allowedMentions: { repliedUser: false }});
+    message.reply({content: "found Twitter link(s)! replaced urls...:\n" + frtssrm, allowedMentions: { repliedUser: false }});
     console.log(`[${currentTime}] Replied to ${message.author.tag}`)
-
-    // messageContent = message.content;
-    // replacedMessage = messageContent.replace(/twitter.com/g, "fxtwitter.com");
-    // message.channel.send(replacedMessage);
   }
+
+  // if (message.content.includes("https://x.com")) {
+
+  //   xm = message.content;
+  //   console.log(`[${currentTime}] X URL found: ${xm}`)
+  //   rxm = xm.replace(/x.com/g, "fixupx.com");
+  //   console.log(`[${currentTime}] Replaced to fixupx.com: ${rxm}`);
+  //   srxm = rxm.match(/https:\/\/fixupx.com\/[a-zA-Z0-9_]*\/status\/[0-9]*/g);
+  //   console.log(`[${currentTime}] Replaced Message preview: ${srxm}`);
+
+  //   tssxrm = srxm.toString();
+  //   frtssxrm = tssxrm.replace(/,/g, "\n");
+
+  //   message.reply({content: "found X link(s)! replaced urls...:\n" + frtssxrm, allowedMentions: { repliedUser: false }});
+  //   console.log(`[${currentTime}] Replied to ${message.author.tag}`)
+  // }
+
 } )
 
 client.login(token);
