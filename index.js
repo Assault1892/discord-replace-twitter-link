54dab84102c0ae03a363604a625552f3334700ef
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2023-10-16 13:33:39 +0900

    fix: :bug: 多分動くようになった

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds ]})
+const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages ]})
 
 client.once(Events.ClientReady, c => {
   console.log("--------------------");
@@ -9,5 +9,21 @@ client.once(Events.ClientReady, c => {
   console.log(`Logged in as ${c.user.tag}!`);
   console.log("--------------------");
 });
+
+client.on(Events.MessageCreate, message => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content.startsWith("test")) {
+    message.channel.send("aaa");
+  }
+
+  if (message.content.includes("https://twitter.com/")) {
+    var messageContent = message.content;
+    var replacedMessage = messageContent.replace("https://twitter.com/", "https://fixtwitter.com");
+    message.channel.send(replacedMessage);
+  }
+} )
 
 client.login(token);
