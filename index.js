11fac345ca73c1a8e66f9151c7b09b2213130d57
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2023-10-16 14:40:12 +0900

    feat: :sparkles: 多分動く

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+require ('date-utils');
 
 const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages ]})
+const date = new Date();
+const currentTime = date.toFormat("YYYY/MM/DD HH24:MI:SS");
 
 client.once(Events.ClientReady, c => {
   console.log("--------------------");
@@ -23,7 +26,19 @@ client.on(Events.MessageCreate, message => {
 
     m = message.content;
     rm = m.replace(/twitter.com/g, "fxtwitter.com");
-    console.log(rm)
+    console.log(`[${currentTime}] Twitter URL found. Replaced to fxtwitter.com: ${rm}`);
+
+    https://fxtwitter.com/SzlyNe_/status/1712654233160573425
+
+    srm = rm.match(/https:\/\/fxtwitter.com\/[a-zA-Z_]*\/status\/[0-9]*/g);
+    console.log(`[${currentTime}] Replaced Message preview: ${srm}`);
+
+    tssrm = srm.toString();
+    frtssrm = tssrm.replace(/,/g, "\n");
+
+    // console.log(frtssrm);
+    message.reply({content: "found twitter links! replaced urls:\n" + frtssrm, allowedMentions: { repliedUser: false }});
+    console.log(`[${currentTime}] Replied to ${message.author.tag}`)
 
     // messageContent = message.content;
     // replacedMessage = messageContent.replace(/twitter.com/g, "fxtwitter.com");
